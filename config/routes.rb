38eadd84c7cbd46f71c4daa18876d4be11e5cd38
YuHan <taiwansmile@gmail.com>
2018-01-25 11:30:30 +0800

    add namespace admin in routes, create admin restraunts controller, add index action and view for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "restaurants#index" # 首頁指向 RestaurantsController 的 index action
+
+  namespace :admin do
+    root "restaurants#index"  # 建立後台路由
+  end
 end
