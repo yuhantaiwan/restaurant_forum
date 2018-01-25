7928830d69bba0583ece1ea1648883749c31a306
YuHan <taiwansmile@gmail.com>
2018-01-25 11:52:31 +0800

    add gem devise, create user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "restaurants#index" # 首頁指向 RestaurantsController 的 index action
 
