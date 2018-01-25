1adb3fa64a667740edeb8abb77e3cd32a3190282
YuHan <taiwansmile@gmail.com>
2018-01-25 11:04:23 +0800

    add root in routes, create restaurants controller, add index action and view for public users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "restaurants#index" # 首頁指向 RestaurantsController 的 index action
 end
