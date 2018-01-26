90dfb80739c3935ab08be9c729d6e5488582c658
YuHan <taiwansmile@gmail.com>
2018-01-26 10:58:04 +0800

    add login process in restaurant controller and update view with login messages

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,8 @@
 class RestaurantsController < ApplicationController
+  # 在每個 Controller Action 開始之前，進行把關。
+  # 當使用者透過網址進入你的網站時，這個方法會檢查從 Route 送進來的 Request，是否來自一個已經登入的使用者。
+  before_action :authenticate_user!
+
   def index
   end
 end
