905c724b85aa010ea2a3a79104de4e0f335c4588
YuHan <taiwansmile@gmail.com>
2018-02-06 11:30:41 +0800

    add restaurants resources in routes for admin

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root "restaurants#index" # 首頁指向 RestaurantsController 的 index action
 
   namespace :admin do
+    resources :restaurants   # resources 會產生一組 URL Helper 和網址，並對應到不同的 Action。
     root "restaurants#index"  # 建立後台路由
   end
 end
